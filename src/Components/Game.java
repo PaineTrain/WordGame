b92c4b15534b9af27006e2793251e4a46262e565
Harrison Paine <harrison.paine@gmail.com>
2012-05-15 18:24:50 -0400

    Added src.Components to path

---
+++ src/Components/Game.java
@@ -0,0 +1,14 @@
+package Components;
+
+import java.util.List;
+import wordGame.Util.Board;
+
+public class Game {
+	private Board gameBoard;
+		
+	
+	public Game(){
+		
+	}
+
+}
---
+++ src/Components/Rules.java
@@ -0,0 +1,11 @@
+package Components;
+
+public class Rules {
+	private static int DEFAULT_MIN_WORD_LENGTH = 3;
+	private static int DEFAULT_TIME_LIMIT = 180000; //milliseconds
+	private int minWordLength;
+	private int timeLimit;
+	
+	
+	
+}
